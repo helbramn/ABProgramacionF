dc93ec5f3a804e727273992d41bdbaac627119af
helbramn <barabasrori@gmail.com>
2024-10-08 12:26:49 +0200

    Agregar archivos de proyecto.

---
+++ ABProgramacionF.cpp
@@ -0,0 +1,12 @@
+﻿// ABProgramacionF.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "ABProgramacionF.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello presi" << endl;
+	return 0;
+}
